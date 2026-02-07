6f23ecf6945a8eff993bc6b39b848bc3f238c6d8
leen-jammal <leen@digital-future.me>
2026-02-08 01:58:43 +0300

    Agentic Code: Project 1769599168935 - Completed 49 steps - 1 files

--- server.js
+++ server.js
@@ -1,22 +1,19 @@
 const express = require('express');
 const app = express();
-
-// Middleware to parse JSON bodies
-app.use(express.json());
-
-// Basic route to test the server
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
-
 const port = 3000;
 
+app.use(express.json());
+
+// Generic error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send({ error: 'Something broke!', message: err.message });
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
